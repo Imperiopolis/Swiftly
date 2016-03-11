ad86d393f55e5d2c9dd82abb28f247fe1eaa7ea2
Cameron Pulsford <cpulsfor@me.com>
2016-03-10 20:17:15 -0500

    Adds some simple unit tests.

--- SwiftlyTests/SwiftlyTests.swift
+++ SwiftlyTests/SwiftlyTests.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 import XCTest
+import Swiftly
 
 class SwiftlyTests: XCTestCase {
+
+    var containerView: UIView!
+    let view = UIView()
+    let offset: CGFloat = 5
+    let negativeOffset: CGFloat = -5
+    let span: CGFloat = 100
+    let percentage: CGFloat = 0.5
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        containerView = UIView(frame: CGRect(x: 0, y: 0, width: span, height: span))
+        containerView.addSubview(view)
     }
     
     override func tearDown() {
@@ -21,16 +30,55 @@ class SwiftlyTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testOriginYPositiveOffset() {
+        view.applyLayout(.Top() + offset)
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        XCTAssert(view.frame.origin.y == offset)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testOriginYNegativeOffset() {
+        view.applyLayout(.Top() - offset)
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        XCTAssert(view.frame.origin.y == negativeOffset)
+    }
+
+    func testOriginXPositiveOffset() {
+        view.applyLayout(.Left() + offset)
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        XCTAssert(view.frame.origin.x == offset)
+    }
+
+    func testOriginXNegativeOffset() {
+        view.applyLayout(.Left() - offset)
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        XCTAssert(view.frame.origin.x == negativeOffset)
+    }
+
+    func testOriginMultiplication() {
+        view.applyLayout(.Width() * 2)
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        XCTAssert(view.frame.size.width == span * 2)
+    }
+
+    func testFlush() {
+        view.applyLayout(.Flush())
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        XCTAssert(view.frame == containerView.frame)
+    }
+
+    func testFlushToMargins() {
+        containerView.layoutMargins = UIEdgeInsets(top: offset, left: offset, bottom: offset, right: offset)
+        view.applyLayout(.FlushToMargins())
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        let expectedSpan = span - (offset * 2)
+        XCTAssert(view.frame == CGRect(x: offset, y: offset, width: expectedSpan, height: expectedSpan))
     }
     
 }
